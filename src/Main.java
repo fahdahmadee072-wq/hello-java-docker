0c33febe2076f093950b3427f2c1f15c822bdc3a
fahdahmadee072-wq <fahdahmadee072@gmail.com>
2025-11-16 20:10:56 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World from Jenkins + Docker!");
+    public static void main(String[] args) throws IOException {
+        int port = 8080;
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+
+        server.createContext("/", exchange -> {
+            String response = "Hello from Java Web Server running inside Docker + Jenkins!";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
+
+        server.start();
+        System.out.println("Server started at http://localhost:" + port);
     }
 }
